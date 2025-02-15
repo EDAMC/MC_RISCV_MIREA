fa79e31faa3d12ac79348500ddffdb6c094698f4
EDAMC <smoskolenko00@yandex.ru>
2025-02-15 13:56:15 +0300

    fix(rclk reset)

--- common_src/sr_driver.sv
+++ common_src/sr_driver.sv
@@ -9,11 +9,9 @@ module sr_driver
 
 logic [15:0] ser;
 logic [3:0] cnt;
-//logic load_enable;
 
 assign sclk 		 = clk;
 assign dio  		 = ser[0];
-//assign rclk 	    = (cnt == 1);
 assign load_enable   = (cnt == 0);
 
 always@ (posedge clk or negedge rst_n_i)
@@ -21,12 +19,18 @@ always@ (posedge clk or negedge rst_n_i)
     else                   cnt <= cnt + 1'b1;
 
 always@ (posedge clk or negedge rst_n_i)
-    if      (!rst_n_i)     ser <= '0;
-    else if (load_enable)  ser <= data_in;             // load parallel data
-    else                   ser <= {ser[0], ser[15:1]}; // serialize parallel data
+    if      (!rst_n_i)     
+        ser <= '0;
+    else if (load_enable)  
+        ser <= data_in;             // load parallel data
+    else 
+        ser <= {ser[0], ser[15:1]}; // serialize parallel data
 
  always@ (posedge clk)
-     if (cnt == 1) rclk <= !rclk;
+    if (!rst_n_i)      
+        rclk <= '0;
+    else if (cnt == 1) 
+        rclk <= !rclk;
 
 
 endmodule
